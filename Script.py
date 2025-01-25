ab5d0b159c6efbb0ab077b9dea775531097da8c6
TmrBotz <tmrbotz@gmail.com>
2025-01-25 18:41:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,7 +244,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b><a href=https://t.me/MRxOfficialSupport>{file_name}</a>
 
 <b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
